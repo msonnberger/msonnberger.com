ab51e0143fe13f65763bdbd46a809cb40c091d2c
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2021-07-22 09:37:17 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "msonnberger.com",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+  ],
+};
