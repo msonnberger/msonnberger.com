d81d2f33f825de0d0e30e51f68f7aff13eef0747
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2022-01-25 11:37:34 +0100

    camelCase for jsx attributes

--- src/components/icons/chevronLeft.js
+++ src/components/icons/chevronLeft.js
@@ -8,9 +8,9 @@ const IconChevronLeft = () => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <polyline points="15 18 9 12 15 6" />
   </svg>
--- src/components/icons/linkedin.js
+++ src/components/icons/linkedin.js
@@ -8,9 +8,9 @@ const IconLinkedin = () => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
     <rect x="2" y="9" width="4" height="12" />
