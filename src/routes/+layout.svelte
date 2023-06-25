e2a263dc1a5b97e72236c7423748b10c28a94c00
Martin Sonnberger <martin@sonnberger.email>
2023-06-25 21:58:47 +0200

    bento setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
 	import '../app.css';
 </script>
 
-<slot />
+<svelte:head>
+	<title>Martin Sonnberger</title>
+</svelte:head>
+
+<div
+	class="w-screen min-h-[100dvh] flex flex-col items-center px-5 bg-slate-50 dark:bg-slate-900 text-slate-800 dark:text-slate-100"
+>
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import GitHub from './GitHub.svelte';
+	import ThemeToggle from './ThemeToggle.svelte';
+</script>
+
+<main class="w-full max-w-2xl">
+	<div class="mt-20 flex justify-between">
+		<h1 class="font-bold text-xl mr-8">Hi, I am Martin Sonnberger 👋</h1>
+		<div class="flex gap-5 text-lg items-center">
+			<a class="block" href="https://github.com/msonnberger" target="_blank">
+				<GitHub />
+			</a>
+			<ThemeToggle />
+		</div>
+	</div>
+	<h2 class="font-light">I do stuff on the web</h2>
+	<hr class="border-slate-300 dark:border-slate-700 mt-4 mb-6" />
+	<h3>Projects</h3>
+	<div class="grid grid-cols-2 gap-3 w-full">
+		<div class="bg-slate-200 h-28 rounded-2xl col-span-2">1</div>
+		<div class="bg-rose-300 h-28 rounded-2xl col-span-2 sm:col-span-1">2</div>
+		<div class="bg-emerald-300 h-28 rounded-2xl col-span-2 sm:col-span-1">3</div>
+		<div class="bg-cyan-300 h-28 rounded-2xl col-span-2">4</div>
+	</div>
+</main>
