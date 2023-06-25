44beccb93c6c224048a62b8473f4628b5bb432a1
Martin Sonnberger <martin@sonnberger.email>
2023-06-25 20:18:52 +0200

    wipe

--- README.md
+++
@@ -1,7 +0,0 @@
-This is my personal website, which you can find at [www.msonnberger.com](https://www.msonnberger.com)
-
-It's built with React and Gatsby; To view it locally, run:
-```bash
-gatsby develop
-```
-The dev server should start on `localhost:8000`
--- src/components/icons/index.js
+++
@@ -1,6 +0,0 @@
-export { default as IconGithub } from './github';
-export { default as IconLinkedin } from './linkedin';
-export { default as IconEmail } from './email';
-export { default as IconExternal } from './external';
-export { default as IconChevronRight } from './chevronRight';
-export { default as IconChevronLeft } from './chevronLeft';
